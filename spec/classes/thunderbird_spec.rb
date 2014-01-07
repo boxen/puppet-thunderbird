adb1176599d5208d468bb4fbc0b810b1ebb41872
rds13 <rds@panther.tique.grp>
2014-01-07 22:40:25 +0100

    update version and add locale

--- spec/classes/thunderbird_spec.rb
+++ spec/classes/thunderbird_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe 'thunderbird' do
 
-  version = '24.0'
+  version = '24.2.0'
+  locale  = 'fr'
 
   it { should contain_class('thunderbird') }
   it { should contain_package("Thunderbird-#{version}").with_provider('appdmg') }
-  it { should contain_package("Thunderbird-#{version}").with_source("http://download-origin.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/#{version}/mac/en-US/Thunderbird%20#{version}.dmg") }
+  it { should contain_package("Thunderbird-#{version}").with_source("http://download-origin.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/#{version}/mac/#{locale}/Thunderbird%20#{version}.dmg") }
 
 end
