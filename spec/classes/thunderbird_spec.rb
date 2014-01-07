bf12e3a8e25faf18f864b174d94815a5b2281f03
rds13 <rds@panther.tique.grp>
2014-01-07 23:07:20 +0100

    correct test for locale

--- spec/classes/thunderbird_spec.rb
+++ spec/classes/thunderbird_spec.rb
@@ -3,10 +3,20 @@ require 'spec_helper'
 describe 'thunderbird' do
 
   version = '24.2.0'
-  locale  = 'fr'
 
   it { should contain_class('thunderbird') }
   it { should contain_package("Thunderbird-#{version}").with_provider('appdmg') }
-  it { should contain_package("Thunderbird-#{version}").with_source("http://download-origin.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/#{version}/mac/#{locale}/Thunderbird%20#{version}.dmg") }
+
+  context "default language" do
+    it { should contain_package("Thunderbird-#{version}").with_source("http://download-origin.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/#{version}/mac/en-US/Thunderbird%20#{version}.dmg") }
+  end
+
+  context "other language" do
+    let(:params) do
+      { :locale => 'fr' }
+    end
+
+    it { should contain_package("Thunderbird-#{version}").with_source("http://download-origin.cdn.mozilla.net/pub/mozilla.org/thunderbird/releases/#{version}/mac/#{locale}/Thunderbird%20#{version}.dmg") }
+  end
 
 end
